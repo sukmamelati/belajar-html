c975e7f4f726c85ba7d35bd53931e49a458d3a12
sukmamelati <deaamanda360p@gmail.com>
2025-05-15 17:49:59 +0700

    Add files via upload

---
+++ pertemuan 7 web sukma melati/soal1Sukma.js
@@ -0,0 +1,8 @@
+// Problem 1 - String Concatenation
+let firstName = 'John';
+let lastName = 'Doe';
+let bornYear = 1998;
+let currentYear = 2023;
+
+let age = currentYear - bornYear;
+console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.');
