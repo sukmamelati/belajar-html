0b8e718701a98e48a731525f7ccfb68b91a7589f
sukmamelati <deaamanda360p@gmail.com>
2025-05-15 20:54:59 +0700

    Add files via upload

---
+++ Tugas pertemuan 7 sukma melati/soal1Sukma.js
@@ -0,0 +1,9 @@
+// Problem 1 - String Concatenation
+let firstName = 'Sukma';
+let lastName = 'Melati';
+let bornYear = 2006;
+
+let currentYear = 2025;
+let age = currentYear - bornYear;
+
+console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.');
---
+++ Tugas pertemuan 7 sukma melati/soal3Sukma.js
@@ -0,0 +1,7 @@
+// Problem 3 - Luas dan Keliling Persegi Panjang
+    let panjang = 20;
+    let lebar = 5;
+    let luasPersegi = panjang * lebar;
+    let kelilingPersegi = 2 * (panjang + lebar);
+    console.log("Luas persegi Panjang = " + luasPersegi);
+    console.log("Keliling persegi Panjang = " + kelilingPersegi);
